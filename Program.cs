6d90e969764f0d169bc791f634c8f9b638dc971c
gajananda32 <113405579+gajananda32@users.noreply.github.com>
2022-10-23 10:07:51 +0530

    Starting the game from postion Zero

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+
+namespace SL1
+{
+    internal class SL1
+    {
+        static void Main(string[] args)
+        {
+            //variable
+            int postion = 0;
+            Random random = new Random();   //Random function to generate values
+            int die = random.Next(0, 1);
+            if(die==postion)
+            {
+                Console.WriteLine("Starting the game with single player at postion: " + postion);
+            }
+        }
+    }
+}
