815ff7209d9670a96f2b9fd03b0ee4154b25aa13
gajananda32 <113405579+gajananda32@users.noreply.github.com>
2022-10-23 10:09:21 +0530

    Adding Dies Values

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace SL2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Random function to generate 1,2,3,4,5,6
+            Random random = new Random();
+            int ran = random.Next(1,7);
+            Console.WriteLine("Player roll the die: ");
+            Console.WriteLine(ran);
+        }
+    }
+}
